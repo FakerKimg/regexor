83cd3e916b33f20053b3bf523ad1208f3216dcbf
FakerKimg <fakefree777+gh000@gmail.com>
2016-06-13 11:46:44 +0800

    fix little bugs and change file name

--- pattern_generator.py
+++ pattern_generator.py
@@ -1,9 +1,62 @@
 from graph_process import *
 
-
+f = open("debugging.mail", "w")
 
 
 # algorithms for finding paths
+
+shortest_paths = []
+for scc in sccs:
+    shortest_paths.append(networkx.shortest_path(scc))
+
+def find_shortest_paths(scc, scc_index):
+    for inward_node in scc.graph["inward_nodes"]:
+        for outward_node in scc.graph["outward_nodes"]:
+            scc.node[inward_node].setdefault("scc_paths", {})
+            scc.node[inward_node]["scc_paths"][outward_node] = shortest_path[scc_index][inward_node][outward_node]
+    return
+
+
+
+def complete_saleman_path(scc, scc_index, p, rns):
+    _path = list(p)
+    rest_nodes = rns
+
+    # for now, simply complete the nodes in order......
+    while True:
+        if len(rns)==0:
+            break
+        node = rest_nodes[0]
+        _path = _path + shortest_path[scc_index][_path[-1]][node]
+        rest_nodes = [node for node in scc.nodes() if (node not in _path) or (node!=outward_node)]
+
+    #rest_nodes = [node for node in scc.nodes() if node not in _path]
+
+    return _path
+
+
+def find_fake_saleman_paths(scc, scc_index):
+    for inward_node in scc.graph["inward_nodes"]:
+        for outward_node in scc.graph["outward_nodes"]:
+            scc.node[inward_node].setdefault("scc_paths", {})
+            _max = 0
+            _eindex = inward_node
+            for eindex, path in shortest_path[scc_index][inward_node].iteritems():
+                if eindex==outward_node:
+                    continue
+                _max = len(path) if _max < len(path) else _max
+                _eindex = eindex if _max < len(path) else _eindex
+
+            _path = shortest_path[scc_index][inward_node][_eindex] if _eindex!=inward_node else [inward_node]
+            rest_nodes = [node for node in scc.nodes() if (node not in _path) or (node!=outward_node)]
+            _path = complete_saleman_path(scc, scc_index, _path, rest_nodes)
+            _path = _path + shortest_path[scc_index][_path[-1]][outward_node]
+
+            scc.node[inward_node].setdefault("scc_paths", {})
+            scc.node[inward_node]["scc_paths"][outward_node] = _path
+
+    return
+
 
 def find_shortest_paths_pairs(scc):
     shortest_paths = []
@@ -13,10 +66,30 @@ def find_shortest_paths_pairs(scc):
 
     return shortest_paths
 
+# find the longest shortest path from source, then find the shortest path to rest nodes
+def find_fake_saleman_paths(scc):
+    shortest_paths = networkx.shortest_path(scc)
+
+    paths = []
+    for inward_node in scc.graph["inward_nodes"]:
+        for outward_node in scc.graph["outward_nodes"]:
+            _max = 0
+            _eindex = -1
+            for eindex, path in shortest_paths[inward_node]:
+                _max = len(path) if _max < len(path) else _max
+                _eindex = eindex if _max < len(path) else _eindex
+
+            
+
+    return paths
+
+
+
 
 
 #else_chars = [chr(i) for i in range(0, 256) if chr(i) not in g.graph["alphabet"]] # assume the alphabet is all ascii, anything_else represents all ascii except those in alphabet of regex(fsm)
-else_chars = [c for c in string.printable if chr(i) not in g.graph["alphabet"]]
+#else_chars = [c for c in string.printable if chr(i) not in g.graph["alphabet"]]
+else_chars = [c for c in string.printable[:-6] if chr(i) not in g.graph["alphabet"]]
 
 def extract_edge_input(edge):
     _inputs = g.edge[edge[0]][edge[1]]["_inputs"]
@@ -52,38 +125,73 @@ for scc in sccs:
         if pattern_str=="":
             scc.node[path[0]]["patterns"][path[-1]] = None
         else:
+            _pattern = parse(pattern_str)
             scc.node[path[0]]["patterns"][path[-1]] = parse(pattern_str)
+            #scc.node[path[0]]["patterns"][path[-1]] = pattern_str
 
 
-def iterate_scc(scc, inward_node, _pattern, output_patterns=[]):
+def iterate_scc(dp, f, scc, inward_node, _pattern, output_patterns=[]):
+    temp_dp = list(dp)
     _patterns = scc.node[inward_node]["patterns"]
     for eindex, scc_pattern in _patterns.iteritems():
-        if scc_pattern == None:
-            out_patterns = iterate_dag_edges(scc, eindex, _pattern, output_patterns)
-            continue
+        new_pattern = None
+        if _pattern==None and scc_pattern==None: # is this logic correct ?
+            pass
+        elif _pattern==None:
+            new_pattern = scc_pattern
+        elif scc_pattern == None:
+            new_pattern = _pattern
+        else:
+            nnn = pattern(pattern(_pattern).reduce()) if isinstance(_pattern, conc) else _pattern
+            new_pattern = pattern((nnn + scc_pattern).reduce()).reduce()
+            if str(new_pattern)=="[!-~]":
+                import pdb;pdb.set_trace()
 
-        new_pattern = (_pattern + scc_pattern)
-        output_patterns = iterate_dag_edges(scc, eindex, new_pattern, output_patterns)
+            #f.write(str(nnn)+" + "+str(scc_pattern)+" == "+str(new_pattern)+"\n")
 
         if eindex in g.graph["finals"]:
             output_patterns.append(new_pattern)
 
+            temp_dp.append(inward_node)
+            temp_dp.append(eindex)
+            #f.write(str(temp_dp) + "\n")
+            f.write(str(new_pattern) + "\n")
+            temp_dp = temp_dp[:-2]
+
+        temp_dp.append(inward_node)
+        output_patterns = iterate_dag_edges(temp_dp, f, scc, eindex, new_pattern, output_patterns)
+        temp_dp = temp_dp[:-1]
+
     return output_patterns
 
 
-def iterate_dag_edges(scc, outward_node, _pattern, output_patterns=[]):
+def iterate_dag_edges(dp, f, scc, outward_node, _pattern, output_patterns=[]):
+    temp_dp = list(dp)
     outward_edges = scc.node[outward_node]["outward_edges"]
     for edge in outward_edges:
         new_scc = sccs[g.node[edge[1]]["scc_index"]]
         edge_input_str = extract_edge_input( (edge[0], edge[1]) )
-        output_patterns = iterate_scc(new_scc, edge[1], _pattern + parse(edge_input_str), output_patterns)
+        edge_pattern = parse(edge_input_str)
+        new_pattern = None
+        if _pattern==None and edge_pattern==None:
+            pass
+        elif _pattern==None:
+            new_pattern = edge_pattern
+        elif edge_pattern==None:
+            new_pattern = _pattern
+        else:
+            new_pattern = _pattern + edge_pattern
+            #f.write(str(_pattern)+" + "+str(edge_pattern)+" == "+str(new_pattern)+"\n")
+
+        temp_dp.append(outward_node)
+        output_patterns = iterate_scc(temp_dp, f, new_scc, edge[1], new_pattern, output_patterns)
+        temp_dp = temp_dp[:-1]
 
     return output_patterns
 
 
 initial = g.graph["initial"]
-out_patterns = iterate_scc(sccs[g.node[initial]["scc_index"]], initial, pattern())
+out_patterns = iterate_scc([], f, sccs[g.node[initial]["scc_index"]], initial, None)
 
-
-
+f.close()
 
