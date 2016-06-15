b1ff1d08b5ec4c5f3a5db82ab2344c3d0b4c1f4c
FakerKimg <fakefree777+gh000@gmail.com>
2016-06-15 21:49:56 +0800

    it should work......

--- gF_transition.py
+++ gF_transition.py
@@ -7,10 +7,11 @@ from regexfsm.fsm import anything_else_cls
 def greenfsm_to_FDFA(gfsm):
     dfa = DFA()
 
+    # no need to add sigma, because the DFA would add sigma automatically when adding transition
     # use string.printable as alphabet
-    dfa.addSigma(string.printable[:-6])
-    #else_chars = [c for c in string.printable[:-6] if c not in g.graph["alphabet"]]
-    else_chars = [c for c in string.printable if c not in gfsm.__dict__["alphabet"]]
+    #dfa.addSigma(string.printable[:-6])
+    else_chars = [c for c in string.printable[:-6] if c not in gfsm.__dict__["alphabet"]]
+    #else_chars = [c for c in string.printable if c not in gfsm.__dict__["alphabet"]]
 
     # add states
     states_mapping = {}
@@ -24,21 +25,46 @@ def greenfsm_to_FDFA(gfsm):
     for final in list(gfsm.__dict__["finals"]):
         dfa.addFinal(states_mapping[final])
 
+    symbol_mapping = {}
+    nrepeat = 1 # num repeat
+    ien = 0 # index of english letters
     # add transitions
     for sindex, transition in gfsm.__dict__["map"].iteritems():
+        temp_mapping = {}
         for _input, eindex in transition.iteritems():
+            if eindex not in temp_mapping.keys():
+                temp_mapping[eindex] = {
+                    "symbol": string.letters[ien] * nrepeat ,
+                    "_inputs": []
+                }
+                ien = ien + 1
+                if ien==len(string.letters):
+                    ien = 0
+                    nrepeat = nrepeat + 1
+
             if isinstance(_input, anything_else_cls):
                 for else_input in else_chars:
-                    dfa.addTransition(states_mapping[sindex], else_input, states_mapping[eindex])
+                    temp_mapping[eindex]["_inputs"].append(else_input)
+                dfa.addTransition(states_mapping[sindex], temp_mapping[eindex]["symbol"], states_mapping[eindex])
             else:
-                dfa.addTransition(states_mapping[sindex], _input, states_mapping[eindex])
+                temp_mapping[eindex]["_inputs"].append(_input)
+                dfa.addTransition(states_mapping[sindex], temp_mapping[eindex]["symbol"], states_mapping[eindex])
 
-    return dfa
+        for eindex, _map in temp_mapping.iteritems():
+            symbol_mapping[_map["symbol"]] = _map["_inputs"]
+
+    return (dfa, symbol_mapping)
 
 
-def iterate_Fregex(Fregex):
+def iterate_Fregex(Fregex, symbol_mapping):
     if isinstance(Fregex, atom):
-        return Fregex.val
+        char_set = ""
+        for cc in symbol_mapping[Fregex.val]:
+            if cc in "[^\-]": # ???????????????
+                char_set = char_set + "\\" + cc
+            else:
+                char_set = char_set + cc
+        return "[" + char_set + "]"
     elif isinstance(Fregex, concat):
         arg1 = iterate_Fregex(Fregex.arg1)
         arg2 = iterate_Fregex(Fregex.arg2)
@@ -54,9 +80,9 @@ def iterate_Fregex(Fregex):
         arg2 = iterate_Fregex(Fregex.arg2)
         if not arg1 and not arg2:
             return None
-        elif not arg1 or arg1=="":
+        elif not arg1:
             return arg2
-        elif not arg2 or arg2=="":
+        elif not arg2:
             return arg1
         return "(" + arg1 + "|" + arg2  + ")"
     elif isinstance(Fregex, star):
@@ -72,10 +98,10 @@ def iterate_Fregex(Fregex):
 
     return None
 
-def FFA_to_regex(fa):
+def FFA_to_regex(fa, symbol_mapping):
     Fregex = fa.regexpSE()
     print "FSM -> Fregex, complete"
-    restr = iterate_Fregex(Fregex)
+    restr = iterate_Fregex(Fregex, symbol_mapping)
 
     return restr
 
