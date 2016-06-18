8fdf65ec4e7dba384d1eba1a89b3fc1a85833c75
FakerKimg <fakefree777+gh000@gmail.com>
2016-06-18 11:58:44 +0800

    finish algorithm, not tested yet......

--- graph_process.py
+++ graph_process.py
@@ -61,7 +61,100 @@ for scc in sccs:
     scc.graph["outward_nodes"] = list(scc.graph["outward_nodes"])
 
 
-# algorithms for finding paths
+# algoritms for finding paths through dag
+condenseg = networkx.condensation(g, sccs)
+
+for edge in dag_edges:
+    sscc_index = g.node[edge[0]]["scc_index"]
+    escc_index = g.node[edge[1]]["scc_index"]
+
+    condenseg.edge[sscc_index][escc_index].setdefault("condensed_edges", [])
+    condenseg.edge[sscc_index][escc_index]["condensed_edges"].append(edge)
+
+# add fake finals (or I should do it on the original graph ??????????????????)
+final_sccs = set()
+for final in g.graph["finals"]:
+    final_sccs.add(g.node[final]["scc_index"])
+final_sccs = list(final_sccs)
+
+for final_scc in final_sccs:
+    condenseg.add_edge(final_scc, str(final_scc)+"_final")
+    condenseg.edge[final_scc][str(final_scc)+"_final"]["_inputs"] = [None]
+
+def simply_bfs(condenseg):
+    initial_scc = g.node[g.graph["initial"]]["scc_index"]
+    condense_tree = networkx.bfs_tree(condenseg, initial_scc)
+    dag_paths = []
+    for final_scc in final_sccs:
+        path = networkx.shortest_path(condense_tree, initial_scc, final_scc)
+        dag_paths.append(path)
+
+    condenseg.graph["dag_paths"] = dag_paths
+    return dag_paths
+
+def find_continue_path(condenseg, condense_tree, leaf):
+    if "continue_path" in condense_tree.node[leaf].keys():
+        return
+    next_node = condenseg.edge[leaf].keys()[0] # choose any one edge ??????
+    continue_edge = condenseg.edge[leaf][next_node]
+    
+    sub_tree = networkx.bfs_tree(condense_tree, next_node)
+    sub_leaves = [node for node in sub_tree.nodes() if sub_tree.out_degree(node)==0 and sub_tree.in_degree(node)==1]
+    for sub_leaf in sub_leaves:
+        if "_final" in sub_leaf:
+            condense_tree.node[leaf]["continue_path"] = [leaf] + networkx.shortest_path(condense_tree, next_node, sub_leaf)
+            return
+        else:
+            find_continue_path(condenseg, condense_tree, sub_leaf)
+
+    condense_tree.node[leaf]["continue_path"] = [leaf] + networkx.shortest_path(condense_tree, next_node, sub_leaves[0])  + condense_tree.node[sub_leaves[0]]["continue_path"][1:] # choose any one sub leaf, sub_leaf is the last of sub_leaves ??????????
+
+    return
+
+def all_bfs_branch(condenseg):
+    initial_scc = g.node[g.graph["initial"]]["scc_index"]
+    condense_tree = networkx.bfs_tree(condenseg, initial_scc)
+    dag_paths = []
+    leaves = [node for node in condense_tree.nodes() if condense_tree.out_degree(node)==0 and condense_tree.in_degree(node)==1]
+    for leaf in leaves:
+        if "_final" in leaf:
+            path = networkx.shortest_path(condense_tree, initial_scc, leaf)
+        else:
+            find_continue_path(condenseg, condense_tree, leaf)
+            path = networkx.shortest_path(condense_tree, initial_scc, leaf)
+            path = path + condense_tree[leaf]["continue_path"][1:]
+        dag_paths.append(path)
+
+    condenseg.graph["dag_paths"] = dag_paths
+    return dag_paths
+
+def all_dag_covers(confenseg):
+    dag_paths = all_bfs_branch(condenseg)
+
+    initial_scc = g.node[g.graph["initial"]]["scc_index"]
+    condense_tree = networkx.bfs_tree(condenseg, initial_scc)
+    rest_edges = [edge for edge in confenseg.edges() if edge not in condense_tree.edges()]
+    for rest_edge in rest_edges:
+        path = networkx.shortest_path(condense_tree, initial_scc, rest_edge[0])
+        _node = rest_edge[1]
+        while True:
+            if condense_tree.out_degree(_node)==0 and condense_tree.in_degree(_node)==1:
+                if "_final" in _node:
+                    path = path.append(_node)
+                else:
+                    path = path + condense_tree.node[_node]["continue_path"]
+                break
+            else:
+                path.append(_node)
+                _node = condense_tree.edge[_node].keys()[0]
+
+        dag_paths.append(path)
+
+    condenseg.graph["dag_paths"] = dag_paths
+    return dag_paths
+
+
+# algorithms for finding paths (in SCC????)
 
 shortest_paths = []
 for scc in sccs:
