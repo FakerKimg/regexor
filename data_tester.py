b48d8d16e6d395a1baea7e54af60e8a2719f264e
FakerKimg <fakefree777+gh000@gmail.com>
2016-06-25 23:41:05 +0800

    change simplybfs to shortest path, add dfs, for comparing with bfs

--- data_tester.py
+++ data_tester.py
@@ -87,7 +87,7 @@ exploitable_regexes["color"] = [
 
 input_types = ["tel", "url", "email", "date", "time", "number", "range", "color"]
 scc_types = ["shortest", "fakesaleman", "radiation"]
-condense_types = ["simplybfs", "allbranch", "allcover"]
+condense_types = ["shortest", "simplybfs", "simplydfs", "allcoverbfs", "allcoverdfs"]
 
 """
 pattern_type = pattern_types[2]
